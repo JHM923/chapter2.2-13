98a9f8eb91423f9cd42fbd3cd0de74d5857c9223
username <1151555861@qq.com>
2021-06-30 15:24:55 +0800

    first commit

---
+++ chapter2.2-13/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace chapter2._2_13
+{
+    class Program
+    {
+        static IEnumerable<int> Fibonacci()
+        {
+            int current = 0;
+            int next = 1;
+            while (true)
+            {
+                yield return current;
+                int oldCurrent = current;
+                current = next;
+                next = next + oldCurrent;
+            }
+        }
+        static void Main(string[] args)
+        {
+            foreach (var value in Fibonacci())
+            {
+              Console.WriteLine(value);
+              if (value > 1000)
+              {
+                  break;
+              }
+            }
+            Console.ReadKey();
+        }
+    }
+}
